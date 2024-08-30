252408b3ac643d1e50b136b8025964d5841f7885
LeonGamerPS1 <fnfmaker666@gmail.com>
2024-08-30 23:13:03 +0200

    Files

--- src/haxe/flixel/fileUtil/FileCreation.hx
+++ src/haxe/flixel/fileUtil/FileCreation.hx
@@ -3,11 +3,19 @@ package src.haxe.flixel.fileUtil;
 import sys.io.File;
 import sys.FileSystem;
 
+/**
+ * File Creation from Haxe-Flixel-Utils.
+ * \n Keep in mind that this is my First Library, and therefore could be bad :( .
+ */
 class FileCreation {
+	/**
+	 * [Reads Files]
+	 * @param path The path to the file that should be read. 
+	 */
 	public static function read(path:String) {
 		if (path == null)
 			throw "NULL ERROR: PATH IS NULL!";
-		return File.read("logo.txt").readAll();
+		return File.read(path).readAll().toString();
 	}
 
 	public static function write(path:String, content:String) {
--- src/haxe/flixel/utils/tools/Main.hx
+++ src/haxe/flixel/utils/tools/Main.hx
@@ -7,7 +7,7 @@ using StringTools;
 class Main {
 	private static function main() {
 		var lol = FileCreation.read("logo.txt");
-        
-		Sys.println(lol);
+        var version = FileCreation.read(".gitver");
+		Sys.println(lol + '\nVersion: $version');
 	}
 }
